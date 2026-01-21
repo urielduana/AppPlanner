c373965904e709bcfa97de390909aecffb601cb3
vadose <uri250602@outlook.es>
2026-01-21 10:27:40 -0600

    DB Prisma migration connection

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -6,4 +6,11 @@ export default defineConfig({
   migrations: {
     path: 'prisma/migrations',
   },
+
+  // To generate the migrations is necessary to set the database connection string.
+  // This connection or directory is different from the app database connection.
+  // This connection is just for DEVELOPMENT PURPOSES.
+  datasource: {
+    url: process.env.DATABASE_URL || 'file:./dev.db',
+  },
 });
