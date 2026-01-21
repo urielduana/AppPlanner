2215673c9a17624735d458dfbfb45eee5313c94b
vadose <uri250602@outlook.es>
2026-01-21 15:34:46 -0600

    Tailwind Install

--- frontend/app/pages/(auth)/login.vue
+++ frontend/app/pages/(auth)/login.vue
@@ -3,6 +3,7 @@
 <template>
   <div>
     <h1>Login Page</h1>
-    <!-- Login form goes here -->
+    <!-- Login form goes here -->}
+    <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
   </div>
 </template>
