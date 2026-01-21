bdd704c7358a051336a5cbfb3a1e0744592488d7
vadose <uri250602@outlook.es>
2026-01-21 01:25:06 -0600

    Auth controller, service and module creation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, TasksModule],
+  imports: [PrismaModule, UsersModule, TasksModule, AuthModule],
   controllers: [],
   providers: [],
 })
