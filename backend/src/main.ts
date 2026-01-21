5d71e4d969d34d752f3b6623de393a25c9449091
vadose <uri250602@outlook.es>
2026-01-21 17:37:30 -0600

    CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*', // Allow all origins - adjust as needed for security
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  });
+  await app.listen(8000);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Strip properties that do not have any decorators in the DTO
@@ -13,6 +19,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 8000);
+  // await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
--- frontend/app/pages/(auth)/login.vue
+++ frontend/app/pages/(auth)/login.vue
@@ -1,9 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useAuthStore } from "~/stores/auth";
+
+const config = useRuntimeConfig();
+console.log(config.public.apiBase);
+
+const auth = useAuthStore();
+
+const email = ref("");
+const password = ref("");
+
+const submit = async () => {
+  await auth.login(email.value, password.value);
+  navigateTo("/dashboard");
+};
+</script>
 
 <template>
   <div>
-    <h1>Login Page</h1>
-    <!-- Login form goes here -->}
-    <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <div>
+      <form @submit.prevent="submit">
+        <input v-model="email" placeholder="Email" />
+        <input v-model="password" type="password" placeholder="Password" />
+        <button>Login</button>
+      </form>
+    </div>
   </div>
 </template>
