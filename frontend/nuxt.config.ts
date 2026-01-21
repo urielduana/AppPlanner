a4fc586ed57eb65bb2f903a2f01dc06a5db36b47
vadose <uri250602@outlook.es>
2026-01-21 16:03:16 -0600

    Install pinia/nuxt

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,6 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  // Modules
+  modules: ["@pinia/nuxt"],
+  // Pinia
+  pinia: {
+    storesDirs: ["./stores/**"],
+  },
+  // Tailwind Config
   css: ["./app/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
