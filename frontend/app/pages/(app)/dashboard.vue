42897839dd0a2c964976758e0c5227c406afb9c0
vadose <uri250602@outlook.es>
2026-01-21 20:50:11 -0600

    Refactor Components

--- frontend/app/pages/(app)/dashboard.vue
+++ frontend/app/pages/(app)/dashboard.vue
@@ -1,48 +1,18 @@
 <script setup lang="ts">
-import { useTasksStore } from "~/stores/tasks";
-
 definePageMeta({
   middleware: "auth",
-});
-
-const tasks = useTasksStore();
-
-// When view is mounted, fetch tasks
-onMounted(() => {
-  tasks.fetchTasks();
 });
 </script>
 
 <template>
   <div>
-    <div>
-      <TaskForm />
-    </div>
-    <div>
-      <h1>My Tasks</h1>
-
-      <div v-if="tasks.loading">Loading...</div>
-      <div v-if="tasks.error">{{ tasks.error }}</div>
-
-      <ul>
-        <li v-for="task in tasks.tasks" :key="task.id">
-          <label>
-            <input
-              type="checkbox"
-              :checked="task.completed"
-              @change="tasks.toggleTask(task.id, !task.completed)"
-            />
-            <span ">
-              {{ task.title }}
-              {{  task.description }}
-              {{ task.dueDate }}
-              {{ task.createdAt }}
-            </span>
-          </label>
-
-          <button @click="tasks.deleteTask(task.id)">Delete</button>
-        </li>
-      </ul>
+    <div class="min-h-screen flex items-center justify-center">
+      <div>
+        <TaskForm />
+      </div>
+      <div>
+        <TaskList />
+      </div>
     </div>
   </div>
 </template>
